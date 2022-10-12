13c65a96d3d1f8984a38384b5db43b206a0456f0
StanislaWka <stas-babitsky@mail.ru>
2022-10-12 13:04:09 +0300

    fix: statistoc service

--- src/services/statistic.ts
+++ src/services/statistic.ts
@@ -5,12 +5,15 @@ class StatisticService {
   async getStatistic() {
     try {
       const usersIds = await User.find({}, { name: 1, surname: 1, _id: 1 }).lean();
-      const posts = await Post.find({}, { title: 1, user: 1, createdAt: 1 })
-        .sort({ createdAt: -1 })
-        .lean();
-      const usersWithPosts = usersIds.map((user) => {
+      const promises = usersIds.map((user) => {
+        return Post.find({ user: user._id }, { title: 1, user: 1, createdAt: 1 })
+          .sort({ createdAt: -1 })
+          .lean();
+      });
+      const userPosts = await Promise.all(promises);
+      const usersWithPosts = usersIds.map((user, index) => {
         // @ts-ignore
-        user.posts = posts.filter((post) => post.user.toString() === user._id.toString());
+        user.posts = userPosts[index];
         return user;
       });
       return usersWithPosts;
